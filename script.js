769ded9efe358ec763aa0cd2ced81d7085f6f900
vihaankapse-png <vihaan.kapse@gmail.com>
2026-08-13 18:39:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function selectClass(className) {
-    document.getElementById("message").textContent =
-        "You selected " + className;
+    window.location.href = "standards.html?class=" +
+        encodeURIComponent(className);
 }
 
+function chooseStandard(standard) {
+    document.getElementById("message").textContent =
+        "You selected " + standard;
+}
